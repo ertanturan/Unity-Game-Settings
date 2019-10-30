4ec83a9258901e6eeac9195452764c86f0081208
Ertan Turan <ertanturan@outlook.com>
2019-10-30 09:44:43 +0300

    scriptable object base created .

---
+++ Assets/GameSettingsTool/Scripts/Enum/AntiAliasing.cs
@@ -0,0 +1,7 @@
+﻿public enum AntiAliasing
+{
+    Two = 2,
+    Four = 4,
+    Six = 6,
+    Eight = 8
+}
---
+++ Assets/GameSettingsTool/Scripts/ScriptableObjects/GameSetting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Game Setting",menuName = "Game Settings/New Game Setting")]
+public class GameSetting : ScriptableObject
+{
+
+    public AntiAliasing AntiAliasing = AntiAliasing.Two;
+
+}
